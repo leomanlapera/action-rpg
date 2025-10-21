e733b6723f2aa7742c5a2742bd3e623af57c2c37
June Leoman Lapera <leomanlapera@gmail.com>
2025-10-21 20:55:08 +0800

    enemy

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -7,6 +7,7 @@ const SPEED = 30.0
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
@@ -33,3 +34,10 @@ func is_player_in_range() -> bool:
 		if distance_to_player < range: 
 			return true
 	return result
+
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player: Player = get_player()
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_los_to_player: bool = not ray_cast_2d.is_colliding()
+	return has_los_to_player
